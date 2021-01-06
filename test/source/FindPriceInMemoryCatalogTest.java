8bbb2df837eefd52c1e7aa59f1218d636c777977
Carmen Lopez <carmen.lopez@gmail.com>
2021-01-06 19:53:03 +0100

    Extracted contract tests for Catalog into their own container.

--- test/source/FindPriceInMemoryCatalogTest.java
+++ test/source/FindPriceInMemoryCatalogTest.java
@@ -1,35 +1,15 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 import java.util.Collections;
 
-public class FindPriceInMemoryCatalogTest {
+public class FindPriceInMemoryCatalogTest extends FindPriceInMemoryCatalogContract {
 
-    @Test
-    void productFound() {
-        Price price = Price.cents(1250);
-        CatalogPrices catalogPrices = catalogWith("::product barcode::", price);
-
-        Price actualPrice = catalogPrices.getPriceForBarcode("::product barcode::");
-
-        Assertions.assertEquals(price, actualPrice);
-    }
-
-    private CatalogPrices catalogWith(String barcode, Price price) {
+    @Override
+    protected CatalogPrices catalogWith(String barcode, Price price) {
         return new InMemoryCatalog(Collections.singletonMap(barcode, price));
     }
 
-    @Test
-    void productNotFound() {
-        CatalogPrices catalogPrices = catalogWithout("::product not found::");
 
-        Price actualPrice = catalogPrices.getPriceForBarcode("::product not found::");
-
-        Assertions.assertEquals(null, actualPrice);
-    }
-
-
-    private CatalogPrices catalogWithout(String barCode) {
+    @Override
+    protected CatalogPrices catalogWithout(String barCode) {
         return new InMemoryCatalog(Collections.singletonMap("::any product different to "+barCode+" ::", Price.cents(123)));
     }
 }
