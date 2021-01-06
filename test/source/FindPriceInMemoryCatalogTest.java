a36e390410cf205015833d9bb9ee1f0e78177ffd
Carmen Lopez <carmen.lopez@gmail.com>
2021-01-06 19:44:41 +0100

    FindPriceInMemoryCatalog tests refactored

--- test/source/FindPriceInMemoryCatalogTest.java
+++ test/source/FindPriceInMemoryCatalogTest.java
@@ -8,19 +8,28 @@ public class FindPriceInMemoryCatalogTest {
     @Test
     void productFound() {
         Price price = Price.cents(1250);
-        CatalogPrices catalogPrices = new InMemoryCatalog(Collections.singletonMap("::product barcode::", price));
+        CatalogPrices catalogPrices = catalogWith("::product barcode::", price);
 
         Price actualPrice = catalogPrices.getPriceForBarcode("::product barcode::");
 
         Assertions.assertEquals(price, actualPrice);
     }
 
+    private CatalogPrices catalogWith(String barcode, Price price) {
+        return new InMemoryCatalog(Collections.singletonMap(barcode, price));
+    }
+
     @Test
     void productNotFound() {
-        CatalogPrices catalogPrices = new InMemoryCatalog(Collections.<String, Price>emptyMap());
+        CatalogPrices catalogPrices = catalogWithout("::product not found::");
 
         Price actualPrice = catalogPrices.getPriceForBarcode("::product not found::");
 
         Assertions.assertEquals(null, actualPrice);
+    }
+
+
+    private CatalogPrices catalogWithout(String barCode) {
+        return new InMemoryCatalog(Collections.<String, Price>emptyMap());
     }
 }
